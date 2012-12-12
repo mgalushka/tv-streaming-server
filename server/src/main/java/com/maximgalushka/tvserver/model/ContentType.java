45ade912e30059875a7822548e942759022e3cff
Maxim Galushka <mgalushka@gmail.com>
2012-12-12 23:28:00 +0200

    added domain model classes for json communications with client

---
+++ server/src/main/java/com/maximgalushka/tvserver/model/ContentType.java
@@ -0,0 +1,14 @@
+package com.maximgalushka.tvserver.model;
+
+/**
+ * <p></p>
+ *
+ * @author Maxim Galushka
+ * @since 12.12.12
+ */
+public enum ContentType {
+
+    DIRECTORY,
+    MEDIA,
+    OTHER
+}
